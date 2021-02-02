2069a96c722c22aeed4660392bdeb226c69a423c
anberjamz <anberjam@yahoo.com>
2021-02-01 19:02:13 -0500

    commit tree 52e89b71e7887b66ea7f6681926713d8038bf3a2

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,15 @@
+let state = {numberOfPresents: 0}
+
+let action = {
+  type: "presents/increase",
+};
+
 export function managePresents(state, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return {numberOfPresents: state.numberOfPresents+1}
+      default:
+        return state
+  }
+
 }
